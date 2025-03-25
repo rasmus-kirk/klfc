4355da6fe6f9d45ae815fccebb30ca299bda20a7
rasmus-kirk <mail@rasmuskirk.com>
2025-03-25 14:32:04 +0100

    Updated to nix flakes

--- default.nix
+++
@@ -1,4 +0,0 @@
-{ ghcVer ? "ghc8104" }:
-let nixpkgs = import ./nix/nixpkgs.nix { inherit ghcVer; };
-in
-  nixpkgs.haskell.packages."${ghcVer}".keyboard-layout-files-creator
---
+++ flake.nix
@@ -0,0 +1,43 @@
+# SPDX-FileCopyrightText: 2021 Serokell <https://serokell.io/>
+#
+# SPDX-License-Identifier: CC0-1.0
+
+{
+  description = "My haskell application";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        haskellPackages = pkgs.haskell.packages.ghc810;
+
+        jailbreakUnbreak = pkg:
+          pkgs.haskell.lib.doJailbreak (pkg.overrideAttrs (_: { meta = { }; }));
+
+        packageName = "keyboard-layout-files-creator";
+      in {
+        packages.${packageName} =
+          haskellPackages.callCabal2nix packageName self {
+            aeson = jailbreakUnbreak haskellPackages.aeson_1_5_6_0;
+          };
+
+        packages.default = self.packages.${system}.${packageName};
+        defaultPackage = self.packages.${system}.default;
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            haskellPackages.haskell-language-server # you must build it with your ghc to work
+            ghcid
+            cabal-install
+          ];
+          inputsFrom = map (__getAttr "env") (__attrValues self.packages.${system});
+        };
+        devShell = self.devShells.${system}.default;
+      });
+}
